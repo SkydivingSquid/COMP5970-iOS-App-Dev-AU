0e7846537a971ac30a9fe93b754f1415c4f6ca8d
Skydiving Squid <87143705+SkydivingSquid@users.noreply.github.com>
2022-09-18 17:21:19 -0500

    Add files via upload

---
+++ su-22_Project1/su-22_Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  su-22_Project1
+//
+//  Created by George Martin on 5/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
